316688214de946dcff2d883d3c458b31384316e7
Benjamin <Benjamin@BenPC-x230t>
2016-02-24 08:57:18 -0500

    RQA Main Preconditions Complete

--- RQAMain.m
+++ RQAMain.m
@@ -1,68 +1,56 @@
 function [RQOut, HM] = RQAMain(signalVector, embed)
-    % need elseif statements for input to only accept integers; can use
-    % floor(x) ~= x as test
-
     % Initialize matricies and variables
     vecLen = length(signalVector);
     lastPossible = vecLen - embed;
     
+    % Error if embed is invalid
+    if embed ~= floor(embed)
+        msg = strcat('Embeded dimension must be a positive integer.');
+        error(msg)
+    end
+        
+    % Calculate delay
+    delay = input(strcat('Time delay within vectors?'));
+    if delay >= 1 && delay == floor(delay)
+        vecDelay = delay;
+    else
+        msg = strcat('Proposed delay vector was not acceptable. Will ', ...
+                      'default to delay=1.');
+        disp(msg)
+        vecDelay = 1;
+    end
+    lastPossible = vecLen - delay*(embed-1); % update last possible
+    
     % Calculate first vector
     first = input(strcat('Time position of first vector?', ...
-        ' Max Possible = ', str2cat(lastPossible), '.'));
-    if first <= lastPossible
+        ' (Max Possible = ', num2str(lastPossible), ') '));
+    if first >= 1 && first <= lastPossible && floor(first)==first
         firstVec = first;
-    elseif floor(first) ~= first
-        msg = strcat('First proposed vector was not integer. Will ', ...
-                      'default to ', num2str(floor(first)), '.');
-        error(msg)
-        firstVec = floor(first);        
     else
-        msg = strcat('First proposed vector will end beyond end of ', ...
-                      'time series. Will default to first possible ', ...
-                      'vector.');
-        error(msg)
+        msg = strcat('First proposed vector is not within acceptable', ...
+                     ' range. Will default to first possible vector.');
+        disp(msg)
         firstVec = 1;
     end    
     
     % Calculate final vector
     last = input(strcat('Time position of last vector?', ...
-        ' Max Possible = ', str2cat(lastPossible), '.'));
-    if lastPossible <= last
+        ' (Max Possible = ', num2str(lastPossible), ')'));
+    if last >= 1 && last <= lastPossible && floor(last)==last
         lastVec = last;
-    elseif floor(last) ~= last
-        msg = strcat('Last proposed vector was not integer. Will ', ...
-                      'default to ', num2str(floor(last)), '.');
-        error(msg)
-        lastVec = floor(last);
     else
-        msg = strcat('Last proposed vector will end beyond end of ', ...
-                      'time series. Will default to last possible ', ...
-                      'vector.');
-        error(msg)
+        msg = strcat('Last proposed vector is not within acceptable ', ...
+                     'range. Will default to last possible vector: ', ...
+                     num2str(lastPossible), '.');
+        disp(msg)
         lastVec = lastPossible;
-    end
-    
-    % Calculate delay
-    delay = input(strcat('Time delay between vectors?'));
-    if delay <= 1
-        delay = 1;
-    elseif floor(delay) ~= delay
-        msg = strcat('First proposed vector will end beyond end of ', ...
-                      'time series. Will default to first possible ', ...
-                      'vector.');
-        error(msg)
-        delay = floor(delay);
-    else
-        msg = strcat('Proposed delay vector was not integer. Will ', ...
-                      'default to ', num2str(floor(delay)), '.');
-        error(msg)
-        lastVec = lastPossible;
-    end
-    
+    end 
+        
     % Calculate vector series
     
     % Fill in table of vectors
     
     % Compare pairwise and output to new array
-    
+    HM = 1;
+    RQOut = 1;
 end
