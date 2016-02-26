6c77b268797c8d06874671952b2a925ed100e31f
Benjamin <Benjamin@BenPC-x230t>
2016-02-26 14:10:53 -0500

    Configuration Finalized

--- RQAMain.m
+++ RQAMain.m
@@ -1,56 +1,94 @@
 function [RQOut, HM] = RQAMain(signalVector, embed)
     % Initialize matricies and variables
     vecLen = length(signalVector);
-    lastPossible = vecLen - embed;
     
-    % Error if embed is invalid
-    if embed ~= floor(embed)
-        msg = strcat('Embeded dimension must be a positive integer.');
-        error(msg)
-    end
+    % Create structure to hold configuration data
+    configStr = struct('lastPossible',      vecLen - embed,...
+                       'delay',             1,...
+                       'first',             1,...
+                       'last',              vecLen - embed,...
+                       'embed',             embed,...
+                       'continueConfig',    'N');
+    
+    % Update configuration and make sure all inputs are valid
+    while configStr.continueConfig ~= 'Y'
+        fprintf(1, '\n');
+        disp('Current Settings:')
+        fprintf(strcat('Embeded Dimension:', '\t', num2str(configStr.embed)))
+        fprintf('\n')
+        fprintf(strcat('First Vector:', '\t', num2str(configStr.first)))
+        fprintf('\n')
+        fprintf(strcat('Last Vector:', '\t', num2str(configStr.last)))
+        fprintf('\n')
+        fprintf(strcat('Delay:', '\t', num2str(configStr.delay)))
+        fprintf('\n')
+        configStr.continueConfig = upper(input('Accept current settings? [Y/N]', 's'));
+        if configStr.continueConfig == 'Y'
+            break
+        end
         
-    % Calculate delay
-    delay = input(strcat('Time delay within vectors?'));
-    if delay >= 1 && delay == floor(delay)
-        vecDelay = delay;
-    else
-        msg = strcat('Proposed delay vector was not acceptable. Will ', ...
-                      'default to delay=1.');
-        disp(msg)
-        vecDelay = 1;
+        % Error if embed is invalid
+        embed = input('Dimension to embed signal into?');
+        if embed == floor(embed) && embed > 0 && embed < vecLen
+            configStr.embed = embed;
+        else
+            disp(strcat('Embeded dimension must be a positive integer',...
+                        'less than the timeseries length. Will default',...
+                        ' to 1.'));
+            configStr.embed = 1;
+        end
+        
+        % Calculate delay
+        fprintf('\n')
+        delay = input(strcat('Time delay within vectors?'));
+        if delay >= 1 && delay == floor(delay)
+            configStr.delay = delay;
+        else
+            fprintf('\n')
+            fprintf(strcat('Proposed delay vector was not acceptable.', ...
+                           ' Will default to delay = 1.'))
+            fprintf('\n')
+            configStr.delay = 1;
+        end
+        configStr.lastPossible = vecLen - delay*(configStr.embed - 1); % Also updates last possible
+        
+        % Calculate first vector
+        first = input(strcat('Time position of first vector?', ...
+            ' (Max Possible = ', num2str(configStr.lastPossible), ') '));
+        if first >= 1 && first <= configStr.lastPossible && floor(first)==first
+            configStr.first = first;
+        else
+            msg = strcat('First proposed vector is not within acceptable', ...
+                         ' range. Will default to first possible vector.');
+            disp(msg)
+            configStr.first = 1;
+        end
+    
+        % Calculate final vector
+        last = input(strcat('Time position of last vector?', ...
+            ' (Max Possible = ', num2str(configStr.lastPossible), ')'));
+        if last >= 1 && last <= configStr.lastPossible && floor(last)==last
+            configStr.last = last;
+        else
+            msg = strcat('Last proposed vector is not within acceptable ', ...
+                         'range. Will default to last possible vector: ', ...
+                         num2str(configStr.lastPossible), '.');
+            disp(msg)
+            configStr.last = configStr.lastPossible;
+        end 
+
     end
-    lastPossible = vecLen - delay*(embed-1); % update last possible
-    
-    % Calculate first vector
-    first = input(strcat('Time position of first vector?', ...
-        ' (Max Possible = ', num2str(lastPossible), ') '));
-    if first >= 1 && first <= lastPossible && floor(first)==first
-        firstVec = first;
-    else
-        msg = strcat('First proposed vector is not within acceptable', ...
-                     ' range. Will default to first possible vector.');
-        disp(msg)
-        firstVec = 1;
-    end    
-    
-    % Calculate final vector
-    last = input(strcat('Time position of last vector?', ...
-        ' (Max Possible = ', num2str(lastPossible), ')'));
-    if last >= 1 && last <= lastPossible && floor(last)==last
-        lastVec = last;
-    else
-        msg = strcat('Last proposed vector is not within acceptable ', ...
-                     'range. Will default to last possible vector: ', ...
-                     num2str(lastPossible), '.');
-        disp(msg)
-        lastVec = lastPossible;
-    end 
         
     % Calculate vector series
     
     % Fill in table of vectors
     
-    % Compare pairwise and output to new array
+    % Compare pairwise and output results to new array
+    
+    % Generate heatmap
     HM = 1;
     RQOut = 1;
 end
+
+% Add third optional input to allow passage of custom config and bypass
+% settings
